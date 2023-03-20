27722c8af153d0717f40753c5f802dea002bd297
May Ann Palencia <mayann.palencia@gmail.com>
2023-03-20 15:22:14 +0800

    feat: revert initialize back to false on detach

--- android/src/main/kotlin/twilio/flutter/twilio_conversations/TwilioConversationsPlugin.kt
+++ android/src/main/kotlin/twilio/flutter/twilio_conversations/TwilioConversationsPlugin.kt
@@ -105,5 +105,6 @@ class TwilioConversationsPlugin : FlutterPlugin {
 
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
         debug("TwilioConversationsPlugin.onDetachedFromEngine")
+        initialized = false
     }
 }
