d23200afd352f658e08322f874e1de085374e72c
May Ann Palencia <mayann.palencia@gmail.com>
2023-03-21 10:48:51 +0800

    test: removing shutdown of conversation client

--- android/src/main/kotlin/twilio/flutter/twilio_conversations/TwilioConversationsPlugin.kt
+++ android/src/main/kotlin/twilio/flutter/twilio_conversations/TwilioConversationsPlugin.kt
@@ -89,6 +89,7 @@ class TwilioConversationsPlugin : FlutterPlugin {
         Api.ParticipantApi.setup(flutterPluginBinding.binaryMessenger, participantApi)
         Api.UserApi.setup(flutterPluginBinding.binaryMessenger, userApi)
         Api.MessageApi.setup(flutterPluginBinding.binaryMessenger, messageApi)
+        flutterClientApi = Api.FlutterConversationClientApi(flutterPluginBinding.binaryMessenger)
         flutterLoggingApi = Api.FlutterLoggingApi(flutterPluginBinding.binaryMessenger)
 
         if (initialized) {
@@ -98,7 +99,6 @@ class TwilioConversationsPlugin : FlutterPlugin {
             Log.d(LOG_TAG, "TwilioConversationsPlugin.onAttachedToEngine")
         }
 
-        flutterClientApi = Api.FlutterConversationClientApi(flutterPluginBinding.binaryMessenger)
 
         initialized = true
     }
